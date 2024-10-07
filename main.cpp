932e7882a96f284f9f475885c52f5d61a3619186
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-07 22:55:23 +0300

    ex 2 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+bool skladne(int x) {
+    if (x <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(x); i++) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
+int* delete_skladne(int* masiv, int size, int& without_s) {
+    without_s = 0;
+    for (int i = 0; i < size; ++i) {
+        if (!skladne(masiv[i])) {
+            without_s++;
+        }
+    }
+
+    int* rev_masiv = new int[without_s];
+    int index = 0;
+    for (int i = 0; i < size; ++i) {
+        if (!skladne(masiv[i])) {
+            rev_masiv[index++] = masiv[i];
+        }
+    }
+
+    return rev_masiv;
+}
+
+int main() {
+    int size;
+    cout << "Enter size for array: ";
+    cin >> size;
+
+    int* masiv = new int[size];
+
+    cout << "Enter elements for array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> masiv[i];
+    }
+
+    int without_s;
+    int* rev_masiv = delete_skladne(masiv, size, without_s); 
+
+    cout << "Your new array: ";
+    for (int i = 0; i < without_s; i++) {
+        cout << rev_masiv[i] << " ";
+    }
+    cout << endl;
+
+    delete[] masiv;
+    delete[] rev_masiv;
+
+    return 0;
 }
